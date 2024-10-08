7fad76cf7d588259cc48b4342fa30840e80e6ceb
Anil Oeguetveren <oegueta@emea.corpdir.net>
2024-10-08 11:44:01 +0200

    initital commit

---
+++ src/main/java/com/training/pipelinedesignpattern/PipelineDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.training.pipelinedesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineDesignPatternApplication.class, args);
+    }
+
+}
